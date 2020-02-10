37184538ce6a259de39d5b70718aa6c49400cbd9
Ben Ng <benngnyc@gmail.com>
2020-02-10 18:00:25 -0500

    fixed config var

--- run_prod.js
+++ run_prod.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const config = require('./config');
 
-const connection = mysql.createConnection(mysql_config);
+const connection = mysql.createConnection(config.mysql_config);
 
 connection.connect();
 
