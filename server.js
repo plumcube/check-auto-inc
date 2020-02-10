9bca6937f6c6b65ff023b2a330b8777b544b7c5a
Ben Ng <benngnyc@gmail.com>
2020-02-10 15:52:04 -0500

    cleaned up code

--- server.js
+++ server.js
@@ -18,26 +18,32 @@
 
 const mysqlssh = require('mysql-ssh');
 
-mysqlssh.connect(
-    {
-        host: '10.211.55.13',
-        user: 'vagrant',
-        password: 'vagrant'
-    },
-    {
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'root',
-        database: 'dev_buzz_db'
-    }
-)
-.then(client => {
-    client.query('SELECT 1+1 AS Result', function (err, results, fields) {
-        if (err) throw err
-        console.log(results[0].Result);
-        mysqlssh.close()
+const ssh_host = {
+    host: '10.211.55.13',
+    user: 'vagrant',
+    password: 'vagrant'
+};
+
+const mysql_config = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'dev_buzz_db'
+};
+
+const connection = mysqlssh.connect(ssh_host, mysql_config);
+
+const execute = query => {
+    connection.then(client => {
+        client.query(query, function (err, results, fields) {
+            if (err) throw err
+            console.log(results[0].Result);
+            mysqlssh.close()
+        })
     })
-})
-.catch(err => {
-    console.log(err)
-})
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+execute('SELECT 1+1 AS Result');
