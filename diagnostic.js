6eb7b5b477b92a4db5d676973cc0a1b0b73dc6a9
Ben Ng <benngnyc@gmail.com>
2020-02-11 11:46:28 -0500

    set and print result

--- diagnostic.js
+++ diagnostic.js
@@ -3,18 +3,26 @@ const mysql = require('mysql');
 const config = require('./config');
 const conn56 = mysql.createConnection(config.original_mysql_config);
 const conn57 = mysql.createConnection(config.mysql_config);
+const result = {};
 
 conn56.connect();
 conn57.connect();
 
+const setResult = (key, val) => {
+    result[key] = val;
+}
+
+const printResult = () => {
+    console.log(result.sort());
+}
+
 const execute = query => {
-    console.log(query);
     conn56.query({sql: query, timeout: 60000}, (err, results, fields) => {
         if (err) throw err
-        console.log(results);
+        setResult(query+'old', results[0].Cnt);
         conn57.query({sql: query, timeout: 60000}, (err, results, fields) => {
             if (err) throw err
-            console.log(results);
+            setResult(query+'new', results[0].Cnt);
         })
     })
 }
@@ -23,10 +31,11 @@ try {
     const data = fs.readFileSync('./data', 'utf8');
     data.split("\n").map(str => {
         if (str) {
-            console.log(str);
             var row = JSON.parse(str);
-            var query = `SELECT '` + row.db + `.` + row.table + `' AS TableName, COUNT(*) FROM ` + row.db + `.` + row.table;
+            var query = `SELECT '` + row.db + `.` + row.table + `' AS TableName, COUNT(*) AS Cnt FROM ` + row.db + `.` + row.table;
             execute(query);
+        } else {
+            printResult();
         }
     });
 } catch (err) {
