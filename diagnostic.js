99b8b09f25570029a3b9558f07f52e081feaa3d9
Ben Ng <benngnyc@gmail.com>
2020-02-11 10:42:13 -0500

    run query to get counts

--- diagnostic.js
+++ diagnostic.js
@@ -1,49 +1,28 @@
-const mysqlssh = require('mysql-ssh');
-const config = require('./config');
 const fs = require('fs');
-const connection = mysqlssh.connect(config.ssh_host, config.mysql_config);
+const mysql = require('mysql');
+const config = require('./config');
+const connection = mysql.createConnection(config.mysql_config);
 
+const execute = query => {
+    connection.then(client => {
+        client.query(query, (err, results, fields) => {
+            if (err) throw err
+            console.log(results);
+        });
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
 
 try {
     const data = fs.readFileSync('./data', 'utf8');
     data.split("\n").map(str => {
-        var obj = JSON.parse(str);
-        console.log(obj);
+        var row = JSON.parse(str);
+        var query = `SELECT ` + row.db + `.` + row.table + `, COUNT(*) FROM ` + row.db + `.` + row.table;
+        execute(query);
     });
 } catch (err) {
   console.error(err)
 }
-
-// let query = `
-//     SELECT t.AUTO_INCREMENT, c.COLUMN_NAME, t.TABLE_SCHEMA, t.TABLE_NAME
-//     FROM
-//     information_schema.tables t
-//     INNER JOIN information_schema.columns c ON c.TABLE_SCHEMA = t.TABLE_SCHEMA AND c.TABLE_NAME = t.TABLE_NAME
-//     WHERE c.extra = 'auto_increment'
-//     AND t.auto_increment != 1
-//     AND t.TABLE_TYPE = 'BASE TABLE'`;
-
-// connection.then(client => {
-//     client.query(query, (err, results, fields) => {
-//         if (err) throw err
-
-//         results.map(row => {
-//             let query2 = `
-//             SELECT '` + row.TABLE_SCHEMA + `', '` + row.TABLE_NAME + `', ` + row.AUTO_INCREMENT + ` AS AUTO_INCREMENT, MAX(` + row.COLUMN_NAME + `)
-//             FROM ` + row.TABLE_SCHEMA + `.` + row.TABLE_NAME + ` HAVING MAX(` + row.COLUMN_NAME + `) + 1 != ` + row.AUTO_INCREMENT;
-//             connection.then(client => {
-//                 client.query(query2, (err, results, fields) => {
-//                     if (err) throw err
-//                     if (results.length) console.log(results);
-//                 });
-//             });
-//         });
-//     });
-// })
-// .catch(err => {
-//     console.log(err)
-// });
-
-
-
 
